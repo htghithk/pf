91c6fa58437044583eddc37c23c041024d66144d
quorploop <>
2025-11-19 21:00:11 +0100

    Update CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.lambdacalc.xyz',
+  site: 'https://linussilberstein.de',
   vite: {
     plugins: [tailwindcss()]
   },
