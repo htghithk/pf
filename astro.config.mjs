dd36816afc8cd4e84eb11c2a26aa4ff68f943970
quorploop <>
2025-11-20 19:37:29 +0100

    Update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lambdacalc.xyz',
+  site: 'https://blog.lambdacalc.xyz',
   vite: {
     plugins: [tailwindcss()]
   },
